42ed1e45737d0a4152e33c0bcf946ff7106ab0c5
Vlad Makarenko <makarenko@mpdl.mpg.de>
2020-03-07 12:55:01 +0100

    +nfs_guest and bindfs

--- scripts/start_vagrant.sh
+++ scripts/start_vagrant.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-nfs_guest
 vagrant up
+# ownership mapping seafile@guest->local user@host (with nfs_guest->bindfs)
+#sudo apt install bindfs
+#sudo bindfs --map=2101/1000:@2101/@1000 /opt/seafile/KEEPER/seafile_keeper_ext /opt/seafile/KEEPER/seafile_keeper_ext
