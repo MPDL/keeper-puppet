fae9afa80a88371da65d47e74e90677e7042e092
Iosif Peterfi <peterfi@mpdl.mpg.de>
2020-05-19 16:21:20 +0200

    Fixed onlyif condition

--- puppet/code/environments/production/modules/keeper/manifests/install.pp
+++ puppet/code/environments/production/modules/keeper/manifests/install.pp
@@ -458,7 +458,7 @@ define keeper::install (
   # onlyif not exists
   exec { 'create_keeper_database':
     command  => "/bin/bash -c \"\\$(mysql -s -N --user=${db['__DB_USER__']} --password=${db['__DB_PASSWORD__']} --database=keeper-db < ${seafile_root}/seafile-server-latest/seahub/keeper/keeper-db.sql)\"",
-    onlyif  => "/bin/bash -c \"[ -z \\$(mysql -s -N --user=${db['__DB_USER__']} --password=${db['__DB_PASSWORD__']} -e \\\"SELECT 1 FROM information_schema.schemata WHERE schema_name='keeper-db'\\\") ];\"",
+    onlyif  => "/bin/bash -c \"[ -z \\$(mysql -s -N --user=${db['__DB_USER__']} --password=${db['__DB_PASSWORD__']} -e \\\"SELECT schema_name FROM information_schema.schemata WHERE schema_name='keeper-db'\\\") ];\"",
     path     => ["/usr/bin", "/bin"],
     require => [ Exec['keeper-deploy-all'] ],
   }
