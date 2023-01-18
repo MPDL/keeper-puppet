7cbc12af6f85d32fa81c05ea10b88295afea4957
Vlad Makrenko <4024310+vmakarenko@users.noreply.github.com>
2023-01-18 16:00:26 +0100

    Update install_puppet5.sh
    set MOD_DIR for installation target, otherwise models will not bee seen from keeper module

--- scripts/install_puppet5.sh
+++ scripts/install_puppet5.sh
@@ -3,19 +3,21 @@
 # install puppet5 + modules, keeper-puppet
 # Ubuntu 20.04.5 LTS
 
+MOD_DIR=/etc/puppet/code/environments/production/modules
+
 sudo su
 apt update
 apt install puppet=5.5.10-4ubuntu3 -y
-puppet module install puppetlabs-mysql -v 10.4.0
-puppet module install puppetlabs-concat -v 7.3.0
-puppet module install puppetlabs-translate -v 2.2.0
-puppet module install puppet-archive -v 6.1.0
-puppet module install puppetlabs-apt -v 9.0.0
-puppet module install puppetlabs-stdlib -v 6.6.0
-puppet module install puppetlabs-inifile -v 4.1.0
-puppet module install puppetlabs-vcsrepo -v 5.3.0
-puppet module install pltraining-dirtree -v 0.3.0
-puppet module install brainsware-resources_deep_merge -v 0.9.1
+puppet module install puppetlabs-mysql -v 10.4.0 -i $MOD_DIR
+puppet module install puppetlabs-concat -v 7.3.0 -i $MOD_DIR
+puppet module install puppetlabs-translate -v 2.2.0 -i $MOD_DIR
+puppet module install puppet-archive -v 6.1.0 -i $MOD_DIR
+puppet module install puppetlabs-apt -v 9.0.0 -i $MOD_DIR
+puppet module install puppetlabs-stdlib -v 6.6.0 -i $MOD_DIR
+puppet module install puppetlabs-inifile -v 4.1.0 -i $MOD_DIR
+puppet module install puppetlabs-vcsrepo -v 5.3.0 -i $MOD_DIR
+puppet module install pltraining-dirtree -v 0.3.0 -i $MOD_DIR
+puppet module install brainsware-resources_deep_merge -v 0.9.1 -i $MOD_DIR
 gem install inifile -v 3.0.0
 exit
 cd $HOME
